fd8fc9e6987b295b8236edcce168cd22cf20465e
Ys Cho <developer@picopress.net>
2026-08-24 23:28:19 +0900

    forget to upload

--- perf.pl
+++ perf.pl
@@ -2,26 +2,30 @@ use strict;
 use warnings;
 use Fcntl qw(O_RDONLY O_RDWR);
 
-sysopen(my $DEVMEM, "/dev/mem", O_RDWR) or die "cannot open /dev/mem";
-sysopen(my $URANDOM, "/dev/urandom", O_RDONLY) or die "kernel may be broken? (can't open /dev/urandom)";
+sysopen(my $DEVMEM, "/dev/mem", O_RDWR) 
+	or die "cannot open /dev/mem";
+sysopen(my $URANDOM, "/dev/urandom", O_RDONLY) 
+	or die "kernel may be broken? (can't open /dev/urandom)";
+
 binmode($DEVMEM); binmode($URANDOM);
 
-# $0	MEMSIZE
-# $1	PERFORM COUNT
-# $2	BLOCK SIZE
-my $MEMSIZE		= $ARGV[0];
-my $COUNT		= $ARGV[1];
+# Args
+# 0		MEMORY SIZE
+# 1		PERFORM COUNT
+# 2		BLOCK SIZE
+my $MEM_SIZE	= $ARGV[0];
+my $PERF_COUNT	= $ARGV[1];
 my $BLOCK_SIZE	= $ARGV[2];
 
 my $payload = "";
 
-for(my $i = 0; $i < $COUNT; ++ $i) {
+for(my $i = 0; $i < $PERF_COUNT; ++ $i) {
 	# Pick a random number between [0, $MEMSIZE)
-	my $random_pos = int(rand($MEMSIZE));
+	my $random_pos = int(rand($MEM_SIZE));
 
-	sysseek($DEVMEM, $random_pos, 0);
 	sysread($URANDOM, $payload, $BLOCK_SIZE, 0);
 
+	sysseek($DEVMEM, $random_pos, 0);
 	syswrite($DEVMEM, $payload, $BLOCK_SIZE);
 }
 
